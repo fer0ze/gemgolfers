6e60b4fb131a13307c1986f7abfd55cb4ac81985
A4abdull <86521904+A4abdull@users.noreply.github.com>
2023-08-30 12:37:44 +0500

    Done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
+    encryptSecretKey: 'prod-gemgolfers',
     firebase: {
         apiKey: "AIzaSyB7DBphKLJephwypk0h20r3aA21YAIJsI4",
         authDomain: "gemtour-4c90a.firebaseapp.com",
