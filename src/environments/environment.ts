1d3652449894e161495f989e5471365372dcbe44
A4abdull <86521904+A4abdull@users.noreply.github.com>
2023-09-11 16:05:57 +0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,9 @@ export const environment = {
     production: false,
     debugging: false,
     encryptSecretKey: 'prod-gemgolfers',
+    uri:'https://gemgolfers-hasura.herokuapp.com/v1/graphql',
+    wssuri:'wss://gemgolfers-hasura.herokuapp.com/v1/graphql',
+    apiKey: 'fercjqjjpgcngydvqoze',
     firebase: {
         apiKey: "AIzaSyB7DBphKLJephwypk0h20r3aA21YAIJsI4",
         authDomain: "gemtour-4c90a.firebaseapp.com",
