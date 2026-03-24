58c19a7fa1df1e9e5411839b70bfaa405ca6b0f8
Feroze <feroze.ahmad@e2esoftware.nl>
2026-03-24 12:40:48 +0500

    update pipeline

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/gemgolfers/browser'));
+app.use(express.static(__dirname + '/dist/gemgolfers'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function (req, res) {
-	res.sendFile(path.join(__dirname + '/dist/gemgolfers/browser/index.html'));
+	res.sendFile(path.join(__dirname + '/dist/gemgolfers/index.html'));
 });
