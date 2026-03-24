9e3ccac69a99f66668bbe33878f16469899393df
ferozeahmad <144215280+ferozeahmad@users.noreply.github.com>
2026-03-24 12:00:20 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/gemgolfers'));
+app.use(express.static(__dirname + '/dist/gemgolfers/browser'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
